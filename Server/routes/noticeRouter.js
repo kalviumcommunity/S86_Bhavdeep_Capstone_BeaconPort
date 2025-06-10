83c46a90fdbc7bafcf238ad96334096684bb513a
Bhavdeep-Sai <bhavdeepsai@gmail.com>
2025-06-10 12:01:05 +0530

    changes

--- Server/routes/noticeRouter.js
+++ Server/routes/noticeRouter.js
@@ -16,10 +16,6 @@ const {
 
 
 router.get("/all", authMiddleware(["SCHOOL", "TEACHER", "STUDENT"]), getAllNotices);
-router.get("/important", authMiddleware(["SCHOOL", "TEACHER", "STUDENT"]), getImportantNotices);
-router.get("/active", authMiddleware(["SCHOOL", "TEACHER", "STUDENT"]), getActiveNotices);
-router.get("/audience/:audience", authMiddleware(["SCHOOL", "TEACHER", "STUDENT"]), getNoticesByAudience);
-router.get("/:id", authMiddleware(["SCHOOL", "TEACHER", "STUDENT"]), getNoticeById);
 
 router.post("/create", authMiddleware(["SCHOOL", "TEACHER"]), createNotice);
 router.put("/:id", authMiddleware(["SCHOOL", "TEACHER"]), updateNoticeWithId);
